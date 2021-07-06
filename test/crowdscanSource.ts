91f4a51d68a260e9e29e4787a86579686477b6a0
lucas Derveaux <lucasderveaux1@gmail.com>
2021-07-06 10:23:50 +0200

    singleObservation in de stream

--- test/crowdscanSource.ts
+++ test/crowdscanSource.ts
@@ -1,6 +1,7 @@
 import { Source } from "../src";
 import { Page } from "../src/lib/Page";
 import { Readable, Writable } from 'stream';
+import { resourceLimits } from "worker_threads";
 const N3 = require('n3');
 const { namedNode, literal } = N3.DataFactory;
 
@@ -15,7 +16,7 @@ export class mySource extends Source {
 
   parseConfig(config: string) {
     this.config = {
-      "entrypoint": "https://production.crowdscan.be/dataapi/gent/veldstraat/data/5",
+      "entrypoint": "https://production.crowdscan.be/dataapi/gent/gent_langemunt/data/5",
       "queryparam": "generatedAtTime"
     }
   }
@@ -29,119 +30,12 @@ export class mySource extends Source {
     //maar toch moet dit erbij staan omdat die anders neit
     r.read = function () { };
 
-    function makeObservation(data) {
-      let inhoud = JSON.parse(data);
-      let header = inhoud["header"];
-      let payload = inhoud["payload"]["regions"];
 
-      let tijd = header['time'];
-      let timedelta = header['timedelta'];
-      let environment = header['environment'];
-
-      let time = new Date(tijd);
-      let writer = new N3.Writer();
-      function makeSingleObservation(writer, headCount, suffix, time, timedelta, environment) {
-        writer.addQuad(
-          namedNode('observation' + suffix),
-          namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
-          namedNode('http://www.w3.org/ns/sosa/Observation')
-        );
-
-        writer.addQuad(
-          namedNode('observation' + suffix),
-          namedNode('http://www.w3.org/ns/sosa/hasFeatureOfInterest'),
-          namedNode('https://production.crowdscan.be/dataapi/gent/environments/' + environment)
-        );
-
-        writer.addQuad(
-          namedNode('observation' + suffix),
-          namedNode('http://www.w3.org/ns/sosa/observedProperty'),
-          namedNode('hoeveelheid_mensen')
-        );
-
-        writer.addQuad(
-          namedNode('observation' + suffix),
-          namedNode('http://www.w3.org/ns/sosa/resultTime'),
-          literal(time)  //hoe ^^xsd:dateTime? -> niet automatisch?
-        );
-
-        writer.addQuad(
-          namedNode('observation' + suffix),
-          namedNode('http://www.w3.org/ns/sosa/hasSimpleResult'),
-          literal(headCount)  //hoe ^^xsd:double -> automatisch
-        );
-
-        writer.addQuad(
-          namedNode('observation' + suffix),
-          namedNode('http://www.w3.org/ns/sosa/phenomenonTime'),
-          writer.blank([
-            {
-              predicate: namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
-              object: namedNode('http://www.w3.org/2006/time#Interval')
-            }, {
-              predicate: namedNode('http://www.w3.org/2006/time#hasBeginning'),
-              object: writer.blank([{
-                predicate: namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
-                object: namedNode('http://www.w3.org/2006/time#Instant')
-              }, {
-                predicate: namedNode('http://www.w3.org/2006/time#inXSDDateTimeStamp'),
-                object: literal(new Date(time - timedelta * 60000))
-              }])
-            },
-            {
-              predicate: namedNode('http://www.w3.org/2006/time#hasEnd'),
-              object: writer.blank([{
-                predicate: namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
-                object: namedNode('http://www.w3.org/2006/time#Instant')
-              }, {
-                predicate: namedNode('http://www.w3.org/2006/time#inXSDDateTimeStamp'),
-                object: literal(time)
-              }])
-            }
-          ])
-        );
-
-        writer.addQuad(
-          namedNode('observation' + suffix),
-          namedNode('http://www.w3.org/ns/sosa/madeBySensor'),
-          namedNode(environment + suffix + '_sensor')
-        );
-      }
-
-      if (payload.length < 1) {
-        console.error("crowdscan api werkt niet goed");
-      } else {
-        if (payload.length == 0) {
-          makeSingleObservation(writer, payload[0], 0, time, timedelta, environment);
-        } else {
-          for (let i = 1; i < payload.length; i++) {
-            makeSingleObservation(writer, payload[i], i, time, timedelta, environment);
-          }
-        }
-      }
-
-      try {
-        writer.end((error, result) => {
-          let tekst:string;
-          tekst = ' ';
-          if (error) {
-            console.log("er is een error: ", error);
-          } else {
-            tekst+=result;
-            console.log('dit werkt? ',tekst);
-          }
-
-        });
-      }catch(e){
-        console.log("er is een grote error: ",e.message);
-      }
-
-    }
 
     r.on('readable', function () {
-      let data: Buffer;
-      data = r.read();
-      makeObservation(data.toString());
+      let data: Buffer = r.read();
+      let ans = data.toString();
+      console.log(ans);
     });
     const p = new Page([], []);
     return p;
@@ -150,7 +44,7 @@ export class mySource extends Source {
   getStreamIfExists() {
     let r = new Readable();
     async function getData() {
-      let val = await fetch('https://production.crowdscan.be/dataapi/gent/veldstraat/data/5'
+      let val = await fetch('https://production.crowdscan.be/dataapi/gent/gent_langemunt/data/5'
         , {
           method: 'GET',
           headers: {
@@ -159,15 +53,131 @@ export class mySource extends Source {
         }
       ).then(response => response.json())
         .catch(err => console.error('error: ', err));
-      let buf = Buffer.from(JSON.stringify(val));
-      r.read = function () {
-        return buf;
+
+
+
+
+
+
+      function makeObservation(data: string) {
+        let rdf;
+
+        let inhoud = JSON.parse(data);
+        let header = inhoud["header"];
+        let payload = inhoud["payload"]["regions"];
+
+        let tijd = header['time'];
+        let timedelta = header['timedelta'];
+        let environment = header['environment'];
+
+        let time = new Date(tijd);
+        let writer = new N3.Writer();
+        function makeSingleObservation(writer, headCount, suffix, time, timedelta, environment) {
+          writer.addQuad(
+            namedNode('observation'),
+            namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+            namedNode('http://www.w3.org/ns/sosa/Observation')
+          );
+
+          writer.addQuad(
+            namedNode('observation'),
+            namedNode('http://www.w3.org/ns/sosa/hasFeatureOfInterest'),
+            namedNode('https://production.crowdscan.be/dataapi/gent/environments/' + environment)
+          );
+
+          writer.addQuad(
+            namedNode('observation'),
+            namedNode('http://www.w3.org/ns/sosa/observedProperty'),
+            namedNode('hoeveelheid_mensen')
+          );
+
+          writer.addQuad(
+            namedNode('observation'),
+            namedNode('http://www.w3.org/ns/sosa/resultTime'),
+            literal(time)  //hoe ^^xsd:dateTime? -> niet automatisch?
+          );
+
+          writer.addQuad(
+            namedNode('observation'),
+            namedNode('http://www.w3.org/ns/sosa/hasSimpleResult'),
+            literal(headCount)  //hoe ^^xsd:double -> automatisch
+          );
+
+          writer.addQuad(
+            namedNode('observation'),
+            namedNode('http://www.w3.org/ns/sosa/phenomenonTime'),
+            writer.blank([
+              {
+                predicate: namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+                object: namedNode('http://www.w3.org/2006/time#Interval')
+              }, {
+                predicate: namedNode('http://www.w3.org/2006/time#hasBeginning'),
+                object: writer.blank([{
+                  predicate: namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+                  object: namedNode('http://www.w3.org/2006/time#Instant')
+                }, {
+                  predicate: namedNode('http://www.w3.org/2006/time#inXSDDateTimeStamp'),
+                  object: literal(new Date(time - timedelta * 60000))
+                }])
+              },
+              {
+                predicate: namedNode('http://www.w3.org/2006/time#hasEnd'),
+                object: writer.blank([{
+                  predicate: namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type'),
+                  object: namedNode('http://www.w3.org/2006/time#Instant')
+                }, {
+                  predicate: namedNode('http://www.w3.org/2006/time#inXSDDateTimeStamp'),
+                  object: literal(time)
+                }])
+              }
+            ])
+          );
+
+          writer.addQuad(
+            namedNode('observation'),
+            namedNode('http://www.w3.org/ns/sosa/madeBySensor'),
+            namedNode(environment + suffix + '_sensor')
+          );
+        }
+
+        if (payload.length < 1) {
+          console.error("crowdscan api werkt niet goed");
+        } else {
+          if (payload.length == 1) {
+            makeSingleObservation(writer, payload[0], 0, time, timedelta, environment);
+          } else {
+            for (let i = 1; i < payload.length; i++) {
+              makeSingleObservation(writer, payload[i], i, time, timedelta, environment);
+            }
+          }
+        }
+
+        try {
+          writer.end((error, result) => {
+            if (error) {
+              console.log("er is een error: ", error);
+            } else {
+              rdf = result;
+            }
+
+          });
+        } catch (e) {
+          console.log("er is een grote error: ", e.message);
+        }
+        return rdf;
       }
-      r.unshift(buf);
+
+      val = makeObservation(JSON.stringify(val));
+
+      val = Buffer.from(val);
+      r.read = function () {
+        return val;
+      }
+      r.unshift(val);
       //push(null) moet anders komt er
-      r.push(buf);
+      r.push(val);
     }
-    setInterval(getData, 6000);
+    setInterval(getData, 60000);
     return r;
   }
 
