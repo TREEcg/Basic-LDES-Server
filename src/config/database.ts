ecfe13cd59864d1f523bc65add9599c49fbb66cb
Kasper Zutterman <kasper.zutterman@ugent.be>
2021-07-07 09:16:22 +0200

    feat: handle unpresent id as empty page

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,6 +3,7 @@ const Sequelize = require('sequelize');
 // Option 2: Passing parameters separately (sqlite)
 module.exports = new Sequelize({
     dialect: 'sqlite',
-    storage: ':memory:'
+    storage: 'C:/GitHub/Basic-LDES-Server/test/database.sqlite'
+    //storage: ':memory:'
     //storage: 'path/to/database.sqlite'
 });
