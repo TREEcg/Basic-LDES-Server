251c78d7ef95e5501be9737a73e36a9c0002c669
Kasper Zutterman <kasper.zutterman@ugent.be>
2021-07-12 16:15:05 +0200

    feat: added 2 new examples using the package

--- Examples/index.js
+++ Examples/index.js
@@ -2,4 +2,4 @@ let app = require('@treecg/basic-ldes-server')
 const AppRunner = app.AppRunner;
 //new AppRunner().runCli(process);
 
-new AppRunner().run("C:/GitHub/Basic-LDES-Server-Examples/config/config.json");
+new AppRunner().run("./config/config.json");
