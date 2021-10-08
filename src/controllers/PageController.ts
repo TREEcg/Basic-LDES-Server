18f65c8be173753f808fb35478c13958b50186a9
brechtvdv <br5cht@hotmail.com>
2021-10-08 15:59:14 +0200

    Catch getPage errors

--- src/controllers/PageController.ts
+++ src/controllers/PageController.ts
@@ -1,25 +1,29 @@
 
 export async function usePageOfSource(req, res) {
-    const sourceMap = res.locals.sourceMap;
-    const path = "/" + req.params[0];
+    try {
+        const sourceMap = res.locals.sourceMap;
+        const path = "/" + req.params[0];
 
-    if (sourceMap.has(path)) {
-        const source = sourceMap.get(path);
+        if (sourceMap.has(path)) {
+            const source = sourceMap.get(path);
 
-        const id = req.params.id;
-        const page = await source.getPage(id);
-        if (page == null) {
-            res.status(404).send(`The page with this ID does not exist`);
+            const id = req.params.id;
+            const page = await source.getPage(id);
+            if (page == null) {
+                res.status(404).send(`The page with this ID does not exist`);
+            } else {
+                const contentType = 'text/turtle'
+                const s = await page.getSerializedPage(contentType);
+                res.setHeader("Content-Type", contentType);
+                if (page.isImmutable) res.setHeader("Cache-Control", "public, max-age=31536000, immutable")
+                s.pipe(res);
+            }
         } else {
-            const contentType = 'text/turtle'
-            const s = await page.getSerializedPage(contentType);
-            res.setHeader("Content-Type", contentType);
-            if (page.isImmutable) res.setHeader("Cache-Control", "public, max-age=31536000, immutable")
-            s.pipe(res);
+            await tryRedirecting(req, res);
         }
-    }
-    else {
-        await tryRedirecting(req, res);
+    } catch (e) {
+        console.log("Something went wrong: " + e);
+        res.status(404).send(`This page does not exist.`);
     }
 }
 
